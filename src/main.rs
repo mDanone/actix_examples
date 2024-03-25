88e81898f18f87d3d199077b420da73ebbd45a9f
mDanone <daniyalbekow@icloud.com>
2024-03-26 00:54:22 +0300

    add another app state changing

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,22 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
-use std::sync::Mutex;
 use std::time::Duration;
+use std::cell::Cell;
 use serde::{Deserialize, Deserializer};
 
 
+#[derive(Clone)]
 struct AppState {
     app_name: String,
-    counter: Mutex<i32>
+    counter: Cell<i32>
 }
+
 
 impl AppState {
     fn new(app_name: &str) -> Self {
         AppState {
             app_name: app_name.to_string(),
-            counter: Mutex::new(0),
+            counter: Cell::new(0),
         }
     }
 }
@@ -49,8 +51,8 @@ fn from_action_type<'de, D>(deserializer: D) -> Result<Option<ActionType>, D::Er
 
 #[get("/{nickname}/{id}")]
 async fn hello(data: web::Data<AppState>, user: web::Path<User>, action: web::Query<Action>) -> impl Responder {
-    let mut counter = data.counter.lock().unwrap();
-    *counter += 1;
+    let count = data.counter.get();
+    data.counter.set(count + 1);
     HttpResponse::Ok()
         .body(
             format!(
@@ -74,7 +76,7 @@ async fn echo(example_json: web::Json<SomeExampleJson>) -> impl Responder {
 }
 
 async fn manual_hello(data: web::Data<AppState>) -> impl Responder {
-    HttpResponse::Ok().body(format!("Hello world: counted {}", data.counter.lock().unwrap()))
+    HttpResponse::Ok().body(format!("Hello world: counted {}", data.counter.get()))
 }
 
 fn scoped_examples(cfg: &mut web::ServiceConfig) {
@@ -88,7 +90,7 @@ fn scoped_examples(cfg: &mut web::ServiceConfig) {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()>{
-    let state = web::Data::new(AppState::new("Example App"));
+    let data = AppState::new("Example App");
 
     let mut builder = SslAcceptor::mozilla_intermediate(
         SslMethod::tls()
@@ -101,7 +103,7 @@ async fn main() -> std::io::Result<()>{
     HttpServer::new(move || {
         App::new()
             .configure(scoped_examples)
-            .app_data(state.clone())
+            .app_data(web::Data::new(data.clone()))
     })
     .workers(4)
     .keep_alive(Duration::from_secs(10))
